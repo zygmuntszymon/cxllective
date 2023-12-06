a6ca533bf2306450d33065af4daf0bff63349f1e
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2023-12-06 20:47:11 +0100

    working on a header (login, register, cart)

--- header.php
+++ header.php
@@ -16,6 +16,26 @@
             <p>"ABYSS" drumkit is already out! Use code ABYSS15 to save 5€</p>
             &nbsp;&asymp; &asymp; &asymp; &asymp; &asymp;
         </div>
+        <div class="">
+            <ul class="">
+                <?php if (is_user_logged_in()) : ?>
+                    <li>
+                        <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">My account</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo esc_url(wp_logout_url(get_permalink(get_option('woocommerce_myaccount_page_id')))) ?>" class="nav-link">Logout</a>
+                    </li>
+                <?php else : ?>
+                    <li>
+                        <a href="<?php echo esc_url(get_permalink(get_option('woocommerce_myaccount_page_id'))) ?>" class="nav-link">Login / Register</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </div>
+        <div class="cart text-right">
+            <a href="<?php echo wc_get_cart_url(); ?>"><span class="cart-icon"></span></a>
+            <span class="items"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+        </div>
         <div id="logo">cxllective</div>
         <nav>
             <a href="#">drumkits</a>
